0ef2742ca4a4c07e14150e9ee8e11c50b037d140
Leonam Pereira Dias <leonam.pd@gmail.com>
2018-10-24 09:47:58 -0300

    project: set pagarme/moment as dependency
    Given that moment doesn't consider timezones. It changes the moment
    dependency to pagarme's moment version that uses _moment-timezone_
    setting `America/Sao_Paulo` as the default timezone. Doing this the
    tests broke because considering the timezone the date objects was
    instantiated wrongly. This commit fixes that problem setting hours to
    these date objects.

--- test/query.js
+++ test/query.js
@@ -9,25 +9,25 @@ describe('Query', function() {
 
 	describe('isBusinessDay', function() {
 		it('should return false on saturday', function() {
-			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 6))).to.eventually.eql(false);
+			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 6, 10))).to.eventually.eql(false);
 		});
 
 		it('should return false on sunday', function() {
-			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 7))).to.eventually.eql(false);
+			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 7, 10))).to.eventually.eql(false);
 		});
 
 		it('should return false on holidays', function() {
-			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 8))).to.eventually.eql(false);
+			return expect(bm.isBusinessDay('brazil', new Date(2016, 1, 8, 10))).to.eventually.eql(false);
 		});
 
 		it('should return false on days with partial financial operation', function() {
-			return expect(bm.isBusinessDay('brazil', new Date(2016, 11, 24))).to.eventually.eql(false);
+			return expect(bm.isBusinessDay('brazil', new Date(2016, 11, 24, 10))).to.eventually.eql(false);
 		});
 	});
 
 	describe('nextBusinessDay', function() {
 		it('should return the correct day', function() {
-			return expect(bm.nextBusinessDay('brazil', new Date(2016, 1, 5))).to.eventually.eql(new Date(2016, 1, 10));
+			return expect(bm.nextBusinessDay('brazil', new Date(2016, 1, 5, 10))).to.eventually.eql(new Date(2016, 1, 10, 10));
 		});
 	});
 });
